385cbb57c6edfe73023171f6a0b397c2c364b703
Utsav Mavani <103135096+codelover405@users.noreply.github.com>
2022-12-28 09:05:41 +0530

    ErrorMessage
    ErrorMessage

--- src/Components/Forms/SignUpForm.js
+++ src/Components/Forms/SignUpForm.js
@@ -1,6 +1,23 @@
 import React from "react";
-import { Formik } from "formik";
+import { Field, Formik, useField, Form, ErrorMessage } from "formik";
 import "./Apps.css";
+import * as Yup from 'yup'
+
+
+const SignUpSchema = Yup.object().shape({
+  firstName: Yup.string().required("This is required!"),
+  email: Yup.string().email("Valid Email").required("This is required!"),
+  phone: Yup.number().required(),
+  password: Yup.string().required().min(6, 'must be of 6 character Log.'),
+  confirmPassword: Yup.string().required().min(6, 'must be of 6 character Log.').oneOf([Yup.ref("password")], "Password must be not same."),
+  termsAndCondtions: Yup.boolean().oneOf([true], "Please accpet tems and condition"),
+  additionalInfoFlag: Yup.boolean(),
+  additionalInfo: Yup.string().when("additionalInfoFlag", {
+    is: true,
+    then: Yup.string().required("This fild is required"),
+  })
+})
+
 const SigninForm = () => {
   return (
     <>
@@ -13,61 +30,12 @@ const SigninForm = () => {
         confirmPassword: "",
         phone: "",
         subscription: "",
-        termsAndCondtions: "",
+        termsAndCondtions: false,
+        additionalInfoFlag: false,
+        additionalInfo: "",
         gender: "",
       }}
-        validate={(values) => {
-          const errors = {};
-          // firstname
-          if (!values.firstName) {
-            errors.firstName = "Required!";
-          }
-          // lastname
-          if (!values.lastName) {
-            errors.lastName = "Required!";
-          }
-          // email
-          if (!values.email) {
-            errors.email = "Required!";
-          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-            errors.email = "Not Valid Email!"
-          }
-          // phone
-          if (!values.phone) {
-            errors.phone = "Required!";
-          } else if (values.phone.length < 10 || values.phone.length > 10) {
-            errors.phone = "Numbar is not 10 Digits";
-          } else if (/^\d{10}$/.test()) {
-            errors.phone = "num";
-          }
-          // password
-          if (!values.password) {
-            errors.password = "Required!";
-          }
-          // confirm Password
-          if (!values.confirmPassword) {
-            errors.confirmPassword = "Required!";
-          } else if (values.password !== values.confirmPassword) {
-            errors.confirmPassword = "Confirm Password is not mach Password";
-          }
-
-
-          // Gender
-          if(!values.gender){
-            errors.gender ="Required!"
-          }
-          // subscription
-          if(!values.subscription){
-            errors.subscription ="Required!"
-          }
-          // 
-          if(!values.termsAndCondtions){
-            errors.termsAndCondtions ="Required!"
-          }
-
-          return errors;
-          // console.log(values);
-        }}
+        validationSchema={SignUpSchema}
 
         onSubmit={(values) => {
           console.log(values);
@@ -76,153 +44,119 @@ const SigninForm = () => {
 
         {(formik) => (
           //onsubmit(formik.handleSubmit) = connect to fomik 
-          <form onSubmit={formik.handleSubmit}>
+
+
+          // <form onSubmit={formik.handleSubmit}>
+          <Form>
             {/* {console.log(formik)} */}
             {console.log(formik.errors)}
             <div className="form-group">
               <label htmlFor="firstName"
               >
                 First Name</label>
-              <input type="text" className="form-control" name="firstName"
+              <Field type="text" name='firstName' className="form-control" />
+              {/* <input type="text" className="form-control" name="firstName"
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                value={formik.values.firstName} />
+                value={formik.values.firstName} /> */}
             </div>
-            <span className="field_error">{formik.errors.firstName}</span>
+            {/* {formik.touched.firstName && formik.errors.firstName && (
+              <span className="field_error">{formik.errors.firstName}</span>)} */}
+            <span className="field_error"><ErrorMessage name="firstName"/></span>
             <div className="form-group mt-2">
               <label htmlFor="lastName">Last Name</label>
-              <input type="text" className="form-control" name="lastName"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.lastName} />
+              <Field type="text" name='lastName' className="form-control" />
             </div>
-            <span className="field_error">{formik.errors.lastName}</span>
+            <span className="field_error"><ErrorMessage name="firstName"/></span>
             <div className="form-group mt-2">
               <label>Gender</label>
               <div>
                 <div className="form-check form-check-inline">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="gender"
-                    id="male"
-                    value="male"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    checked={formik.values.gender === "male"}
-                  />
+                  <Field type="radio" value="male" id="male" name='gender' className="form-check-input" />
                   <label className="form-check-label" htmlFor="male">
                     Male
                   </label>
                 </div>
                 <div className="form-check form-check-inline">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="gender"
-                    id="female"
-                    value="female"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    checked={formik.values.gender === "female"}
-                  />
+                  <Field type="radio" value="female" id="female" name='gender' className="form-check-input" />
                   <label className="form-check-label" htmlFor="female">
                     Female
                   </label>
                 </div>
                 <div className="form-check form-check-inline">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="gender"
-                    id="other"
-                    value="other"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    checked={formik.values.gender === "other"}
-                  />
+                  <Field type="radio" value="other" id="other" name='gender' className="form-check-input" />
                   <label className="form-check-label" htmlFor="other">
                     Other
                   </label>
                 </div>
-                <span className="field_error">{formik.errors.lastName}</span>
               </div>
             </div>
+                <span className="field_error"><ErrorMessage name="gender" /></span>
             <div className="form-group mt-2">
               <label htmlFor="email">Email</label>
-              <input type="text" className="form-control" name="email"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email} />
+              <Field type="text" name='email' className="form-control" id="email" />
             </div>
-            <span className="field_error">{formik.errors.email}</span>
+            {/* {formik.touched.email && formik.errors.email && (
+              <span className="field_error">{formik.errors.email}</span>)} */}
+            <span className="field_error"><ErrorMessage name="email" /></span>
             <div className="form-group mt-2">
               <label htmlFor="phone">Phone Number</label>
-              <input type="text" className="form-control" name="phone"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.phone} />
+              <Field type="text" name='phone' className="form-control" id="phone" />
             </div>
-            <span className="field_error">{formik.errors.phone}</span>
+            <span className="field_error"><ErrorMessage name="phone" /></span>
             <div className="form-group mt-2">
               <label htmlFor="password">Password</label>
-              <input type="password" className="form-control" name="password"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.password} />
+              <Field type="password" name='password' className="form-control" id="password" />
             </div>
-            <span className="field_error">{formik.errors.password}</span>
+            <span className="field_error"><ErrorMessage name="password" /></span>
             <div className="form-group mt-2">
               <label htmlFor="confirmPassword">Confirm Password</label>
-              <input
-                type="password"
-                className="form-control"
-                name="confirmPassword"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.confirmPassword}
-              />
+              <Field type="password" name='confirmPassword' className="form-control" id="confirmPassword" />
             </div>
-            <span className="field_error">{formik.errors.confirmPassword}</span>
-
+            <span className="field_error"><ErrorMessage name="confirmPassword" /></span>
             <div className="form-group mt-2">
-              <label htmlFor="confirmPassword">Subscription</label>
-              <select className="form-control" name="subscription"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.subscription}
-              >
-                <option value="">Select</option>
+              <label htmlFor="subscription">Subscription</label>
+              <Field name="subscription" component="select" className="form-select">
+                <option value="" disabled>Select</option>
                 <option value="subscription-1">Free</option>
                 <option value="subscription-2">Pro</option>
                 <option value="subscription-3">Enterprise</option>
-              </select>
+              </Field>
             </div>
-            <span className="field_error">{formik.errors.subscription}</span>
-
+            <span className="field_error"><ErrorMessage name="subscription" /></span>
             <div className="form-group mt-2">
               <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="termsAndCondtions"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.termsAndCondtions}
-                />
+                <Field type="checkbox" name='additionalInfoFlag' className="form-check-input" id="additionalInfoFlag" />
+                <label className="form-check-label" htmlFor="termsAndCondtions">
+                  AdditionalInfoFlag.
+                </label>
+              </div>
+              <span className="field_error"><ErrorMessage className="field_error" name="additionalInfoFlag" /></span>
+            </div>
+            {formik.values.additionalInfoFlag &&
+              <div className="form-group mt-2">
+                <label htmlFor="additionalInfo">Enter Additioal Information </label>
+                <Field as="textarea" name='additionalInfo' className="form-control" value={formik.additionalInfo} id="additionalInfo" />
+                {/* {formik.touched.additionalInfo && formik.errors.additionalInfo && (
+                  <span className="field_error">{formik.errors.additionalInfo}</span>)} */}
+                <span className="field_error"><ErrorMessage name="additionalInfo" /></span>
+              </div>
+            }
+            <div className="form-group mt-2">
+              <div className="form-check">
+                <Field type="checkbox" name='termsAndCondtions' className="form-check-input" id="termsAndCondtions" />
                 <label className="form-check-label" htmlFor="termsAndCondtions">
                   Accept terms and conditions.
                 </label>
               </div>
-              <span className="field_error">{formik.errors.termsAndCondtions}</span>
+              <span className="field_error"><ErrorMessage name="termsAndCondtions" /></span>
             </div>
-
             <div className="d-grid mt-2">
               <button type="submit" className="btn btn-primary btn-block">
                 Sign Up
               </button>
             </div>
-          </form>
+          </Form>
         )}
       </Formik>
     </>
